327f12652c83028e18dc91d36b46a9d3ef734e1f
natal <mikh.nataly@mail.ru>
2020-02-24 12:25:16 -0700

    test_check_btn_add_to_basket for differet language

---
+++ test_items.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+import pytest
+from selenium.webdriver.common.by import By
+
+@pytest.mark.usefixtures("browser")
+class TestItems:
+    def test_check_btn_add_to_basket(self, browser):
+        browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+        time.sleep(2)
+        assert len(browser.find_elements( By.CLASS_NAME, "btn-add-to-basket")) == 1
